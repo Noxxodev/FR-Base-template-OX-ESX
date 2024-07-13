de2629e9249a90229eae7ffab551d291ff50a80b
Noxxo Dev <148816839+Noxxodev@users.noreply.github.com>
2024-07-13 13:48:55 +0200

    Wind studio

--- resources/[server]/[utils]/ox_admin/config.lua
+++ resources/[server]/[utils]/ox_admin/config.lua
@@ -7,7 +7,7 @@ Config.AdminLogging = true
 Config.ParametreDuServeur = true
 Config.MessageStaffActived = false
 
-Config.Webhook = 'https://discord.com/api/webhooks/1203007194048765994/Rl8LNxzc_jJj8NqbWs8wauARWmvyYhbtNq16Otc7AG8Ab0hRSjAhec_kgcS7B65HcpNZ'
+Config.Webhook = ''
 
 Config.GroupsOpenMenu = {
     'admin'
